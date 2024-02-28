e777c913ca4821b4144564930ecf9d6ac8195a28
Vaishnaviv2002 <93897993+Vaishnaviv2002@users.noreply.github.com>
2024-02-28 15:25:19 +0530

    post

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 const app=express()
- app.get('/',(req,res)=>
- {
-    res.send('Express vercel app response')
- })
+app.get('/',(req,res)=>
+{
+   res.send('Hello World')
+})
+
+app.post('/addItemsToCart', async (req, res) => {
+    const { cart_no, product_id } = req.body;
+    const id = {};
+   console.log(cart_no,product_id);
+  
+    res.json(`${cart_no} ${product_id} Successfully inserted the Item`);
+  });
 
 app.listen(5000,()=>
 {
