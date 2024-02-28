64f3ab6dcf0e8eb614ad4660f90e1b4cf8b39b95
Vaishnaviv2002 <93897993+Vaishnaviv2002@users.noreply.github.com>
2024-02-28 16:10:34 +0530

    changedbyg

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-import express from 'express'
-const app=express()
-app.get('/',(req,res)=>
-{
-   res.send('Hello World')
-})
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Use CORS middleware
+app.use(cors());
+
+// Use body-parser middleware to parse JSON
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.post('/addItemsToCart', async (req, res) => {
-    const { cart_no, product_id } = req.body;
-    const id = {};
-   console.log(cart_no,product_id);
-  
-    res.json(`${cart_no} ${product_id} Successfully inserted the Item`);
-  });
+    try {
+        const { cart_no, product_id } = req.body;
+        console.log(cart_no, product_id);
+        res.json(`${cart_no} ${product_id} Successfully inserted the Item`);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
-app.listen(5000,()=>
-{
-    xonsole.log('app is running')
-})
+app.listen(5000, () => {
+    console.log('app is running');
+});
